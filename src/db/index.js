84ea541429991f6a77f3681436e23bd51160a08f
luck-02 <luck.ndzamba@gmail.com>
2025-07-11 14:39:05 +0000

    workflow

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 
 const pool = mariadb.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3307,
+  port: process.env.DB_PORT || 3311,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
